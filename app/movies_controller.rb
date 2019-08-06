8f1f2258355b395eca0b69fc57cef539be5ab416
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-06 02:30:51 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -70,10 +70,9 @@ def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
   attributes = {:title => "Awesome FLick"}
-  Movie.find_by(attributes).update(:title => "Even Awesome Flick")
+  Movie.find_by(attributes)
+  Movie.update(:title => "Even Awesome Flick")
   Movie.save
-  __
-  __
 end
 
 def can_update_using_update_method
